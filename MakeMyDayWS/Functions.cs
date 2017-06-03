da71144dcb3b55624e96ad9581c47a8610cb03c9
Przemysław Muś <przemyslaw.krzysztof.mus@gmail.com>
2017-06-03 04:20:35 +0200

    Dodanie funkcji

--- MakeMyDayWS/Functions.cs
+++ MakeMyDayWS/Functions.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Web;
+﻿using System.Collections.Generic;
 
 namespace MakeMyDayWS
 {
@@ -13,6 +8,30 @@ namespace MakeMyDayWS
         public static bool Test()
         {
             return true;
-        }       
+        }
+
+        public static int DodajZnajomego(string nick)
+        {
+            // 0 - Dodano znajomego
+            // 1 - Użytkownik jest już na liście znajomych
+            // 2 - Nie ma takiego użytkownika 
+
+
+            return 0;
+        }
+
+        public static bool UtworzWydarzenie (Event wydarzenie)
+        {
+             
+
+            return true;
+        }
+
+        public static List<User> ZwrocListe (int id)
+        {
+
+            
+            return null;
+        }
     }
 }

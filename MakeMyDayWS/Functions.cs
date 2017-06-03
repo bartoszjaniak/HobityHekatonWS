52b6d6e0419915855cb1f5f85fa2aa175ab38806
Bartosz Janiak <bartoszjaniak@outlook.com>
2017-06-03 04:44:11 +0200

    Poprawki

--- MakeMyDayWS/Functions.cs
+++ MakeMyDayWS/Functions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace MakeMyDayWS
 {
@@ -14,11 +15,11 @@ namespace MakeMyDayWS
         public static List<Event> WyswietlWydarzenia(User user){
             
             List<Event> Lista = new List<Event>();
-            Lista.Add(new Event() { Description = "Testowe wydarzenie" });
-            Lista.Add(new Event() { Description = "Testowe wydarzenie2" });
+            Lista.Add(new Event() { Description = "Testowe wydarzenie", Latitude = 123.3123, Longitude = 12.123, Icon = 1, IsPublic = true, Orgaznizer = new User() { Nick = "asdasd" }, StartDate = DateTime.Now, Name = "Ognisko", TimeInHours = 1 });
+            Lista.Add(new Event() { Description = "Testowe wydarzenie", Latitude = 12.3123, Longitude = 123.123, Icon = 1, IsPublic = true, Orgaznizer = new User() { Nick = "asdasesdfd" }, StartDate = DateTime.Now, Name = "Grill", TimeInHours = 1 });
+            
             return Lista;
         }
-
 
 
         public static int DodajZnajomego(string nick)
@@ -26,22 +27,19 @@ namespace MakeMyDayWS
             // 0 - Dodano znajomego
             // 1 - Użytkownik jest już na liście znajomych
             // 2 - Nie ma takiego użytkownika 
-
-
             return 0;
         }
 
         public static bool UtworzWydarzenie (Event wydarzenie)
-        {
-             
-
+        {  
             return true;
         }
 
-        public static List<User> ZwrocListe (int id)
+        public static List<User> ZwrocListeZnajomych (User user)
         {
-
-            
+            List<User> Lista = new List<User>();
+            Lista.Add(new User() { ID = 2, Nick = "Klakier" });
+            Lista.Add(new User() { ID = 75, Nick = "Gamoń" });
             return null;
         }
 
